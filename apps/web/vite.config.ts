cfd95feb5df6c3915c9761cf03ba66c3a2c1c4d3
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2026-04-15 20:16:58 +0800

    feat: revert to no RSCs because they don't have any benefits most of the time

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
-import rsc from "@vitejs/plugin-rsc";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite-plus";
 
@@ -112,11 +111,7 @@ export default defineConfig({
           staticNodeEnv: true,
         },
       },
-      rsc: {
-        enabled: true,
-      },
     }),
-    rsc(),
     viteReact(),
     /** @see {@link https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md#react-compiler} */
     babel({
