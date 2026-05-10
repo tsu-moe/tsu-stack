367649ef9ef068fccf0ed5590e5d5584b7d2fb4d
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2026-05-10 17:15:02 +0800

    refactor(seo): add SEO factory lib to apps/web

--- apps/web/src/routes/{-$locale}/(root-layout)/index.tsx
+++ apps/web/src/routes/{-$locale}/(root-layout)/index.tsx
@@ -1,7 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { generateAppSeo } from "@/shared/lib/seo";
+
 import { HomePage } from "@/pages/home";
 
+import { appConfig } from "@/config/app.config";
+
 export const Route = createFileRoute("/{-$locale}/(root-layout)/")({
+  head: ({ params }) =>
+    generateAppSeo({
+      alternates: {
+        canonicalPath: "/",
+        locale: params.locale,
+      },
+      description: appConfig.site.description,
+    }),
   component: HomePage,
 });
