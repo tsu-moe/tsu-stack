65490b84ff7f9909a3a6590009f1dac503d7e3dd
Luzefiru <105530193+Luzefiru@users.noreply.github.com>
2026-09-13 00:20:33 +0800

    refactor(auth): remove obsolete account issuer

--- packages/db/src/schema/auth.schema.ts
+++ packages/db/src/schema/auth.schema.ts
@@ -1,5 +1,5 @@
 import { defineRelationsPart } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index, uniqueIndex } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -37,7 +37,6 @@ export const account = pgTable(
   "account",
   {
     id: text("id").primaryKey(),
-    issuer: text("issuer").notNull(),
     accountId: text("account_id").notNull(),
     providerId: text("provider_id").notNull(),
     userId: text("user_id")
@@ -55,10 +54,7 @@ export const account = pgTable(
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull()
   },
-  (table) => [
-    uniqueIndex("account_issuer_accountId_uidx").on(table.issuer, table.accountId),
-    index("account_userId_idx").on(table.userId)
-  ]
+  (table) => [index("account_userId_idx").on(table.userId)]
 );
 
 export const verification = pgTable(
